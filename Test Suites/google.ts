4dbc9579e70800c5bd867a9200367e8abb2060f9
Wajid-Husayn <wajid.husayn@gmail.com>
2023-07-16 22:43:03 +0500

    test message

--- Test Suites/google.ts
+++ Test Suites/google.ts
@@ -12,17 +12,24 @@
    <rerunImmediately>false</rerunImmediately>
    <testSuiteGuid>ee68b56c-6c21-49b3-a0d0-5f7f67d72d3b</testSuiteGuid>
    <testCaseLink>
-      <guid>c8a8ff1a-ddbb-47e3-bbde-dbe4c9e44927</guid>
+      <guid>3f91ee98-ff88-4c7a-9577-50cc58136ac8</guid>
       <isReuseDriver>false</isReuseDriver>
       <isRun>true</isRun>
       <testCaseId>Test Cases/Example/1</testCaseId>
       <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
    </testCaseLink>
    <testCaseLink>
-      <guid>71b65607-22d8-42f9-88b0-8126282a91d7</guid>
+      <guid>d6ce3d44-9882-478f-bd73-151ffda3c2bd</guid>
       <isReuseDriver>false</isReuseDriver>
       <isRun>true</isRun>
       <testCaseId>Test Cases/Example/2</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>f70ff4f2-a65e-4ef7-90c7-ca01efd20e63</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Example/3</testCaseId>
       <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
    </testCaseLink>
 </TestSuiteEntity>
